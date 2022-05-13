a865b778c0a6b7c82eb2351de027198fe31db5be
Povilas Kirna <povilas.kirna@gmail.com>
2022-05-13 23:53:31 +0200

    Initial page design

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import React from "react";
+import Head from "next/head";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@emotion/react";
+import theme from "../src/theme";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = (props) => {
+	const { Component, pageProps } = props;
 
-export default MyApp
+	return (
+		<React.Fragment>
+			<ThemeProvider theme={theme}>
+				<style jsx global>{`
+					body {
+						background-color: ${theme.palette.primary.highlight};
+					`}</style>
+
+				<Head>
+					<title>Weather - PK</title>
+					<meta
+						name="viewport"
+						content="minimum-scale=1, initial-scale=1, width=device-width"
+					/>
+				</Head>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</React.Fragment>
+	);
+};
+
+export default MyApp;
---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#186048",
+			lighter: "#189078",
+			lighter_2: "#30a878",
+			lighter_3: "#90d890",
+			light: "#a8f0a8",
+			highlight: "#d8f0c0",
+		},
+	},
+});
+
+export default theme;
